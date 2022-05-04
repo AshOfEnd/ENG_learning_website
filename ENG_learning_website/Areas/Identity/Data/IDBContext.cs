c0a56b1e1f2349ed9a558dac9679693219d3193f
wloda <AshOfEnd@wp.pl>
2022-05-04 02:20:56 +0200

    update Login via Google

---
+++ ENG_learning_website/Areas/Identity/Data/IDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ENG_learning_website.Data;
+
+public class IDBContext : IdentityDbContext<IdentityUser>
+{
+    public IDBContext(DbContextOptions<IDBContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- ENG_learning_website/Program.cs
+++ ENG_learning_website/Program.cs
@@ -1,8 +1,19 @@
 using ENG_learning_website.Data;
 using ENG_learning_website.Data.services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IDBContextConnection");;
+
+builder.Services.AddDbContext<IDBContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<IDBContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -11,6 +22,16 @@ builder.Services.AddDbContext<DBContext>
                 options.UseSqlServer(
                  builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAuthentication()
+    .AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
+
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<ICourseService, CourseService>();
 var app = builder.Build();
 
@@ -26,11 +47,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}",
+    app.MapRazorPages()
+    );
 
 app.Run();
