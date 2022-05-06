74f561f2b9fc39119af44fbdfb964755d18bb79c
Natalia230800 <natalciask@o2.pl>
2022-05-06 19:29:44 +0200

    Zmiany 3

---
+++ ENG_learning_website/Controllers/Fiszki.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ENG_learning_website.Controllers
+{
+    public class Fiszki : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ENG_learning_website/Controllers/zad1.cs
+++ ENG_learning_website/Controllers/zad1.cs
@@ -1,23 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ENG_learning_website.Data;
+using ENG_learning_website.Models;
 
 namespace ENG_learning_website.Controllers
 {
+
     public class zad1 : Controller
     {
-        // GET: /HelloWorld/
+        private readonly DBContext _context;
 
-        public IActionResult Index()
+        public zad1(DBContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+           
+            
             return View();
         }
-       
-
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
+              
     }
 }
